348bd74b9ed48a07c386910ad9747a2f3fa8646c
Vina del Mar <vina@duoc.acad>
2024-09-06 14:45:11 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'eventura',
+  webDir: 'www'
+};
+
+export default config;
