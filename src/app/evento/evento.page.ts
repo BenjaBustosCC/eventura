6a2fe396936ac9a40da4506fca0c18be88fe1aa5
BenjaBustosCC <ben.bustosm@duocuc.cl>
2024-09-10 02:26:55 -0300

    nose que cambié pero hago el commit por si acaso xd intentando cambiar el color del boton de retroceso. Al menos el botón navega xdxd

--- src/app/evento/evento.page.ts
+++ src/app/evento/evento.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-evento',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventoPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  onBackButtonPressed() {
+    this.router.navigate(['/tabs/home'])
+  }
 }
