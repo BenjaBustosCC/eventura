489776177b4c12702c15e33591c0567859b7bd85
BenjaBustosCC <ben.bustosm@duocuc.cl>
2024-09-11 16:27:06 -0300

    VISTA AGREGAR EVENTO KEDA POKITOOOO

--- src/app/tabs/add-event/add-event.page.ts
+++ src/app/tabs/add-event/add-event.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-add-event',
   templateUrl: './add-event.page.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEventPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  onBackButtonPressed() {
+    this.router.navigate(['/tabs/home'])
+  }
 }
