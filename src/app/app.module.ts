cbab4f868a0a36650fd3ad1ad4bf2918de752332
GisselleAliagaSaavedra <gias0201@gmail.com>
2024-10-26 22:16:39 -0300

    Creacion de auth en firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"eventura-c026d","appId":"1:123073664431:web:ff40262232c6fac4bfcb5a","storageBucket":"eventura-c026d.appspot.com","apiKey":"AIzaSyAUS26c755_78ujPqVM8lt3-9UeAv5sIgM","authDomain":"eventura-c026d.firebaseapp.com","messagingSenderId":"123073664431","measurementId":"G-CRTWM641BD"})), provideAuth(() => getAuth()), provideDatabase(() => getDatabase()), provideRemoteConfig(() => getRemoteConfig())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/managers/SessionManager.ts
+++ src/managers/SessionManager.ts
@@ -24,7 +24,7 @@ export class SessionManager {
     }
     getUser(): string {
         return this.temporaryUser;
-      }
+}
 
     
 }
