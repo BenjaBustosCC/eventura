61afe5a786789156979d257f631b7eee25bc20ce
BenjaBustosCC <ben.bustosm@duocuc.cl>
2024-11-07 05:35:47 -0300

    agregado el storageService y signout

--- src/app/tabs/profile/profile.page.ts
+++ src/app/tabs/profile/profile.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { SessionManager } from 'src/managers/SessionManager';  // Importar el SessionManager
+import { Router } from '@angular/router';
+import { UserLogoutUseCase } from 'src/app/use-cases/user-logout.use-case';
+import { CancelAlertService } from 'src/managers/CancelAlertService';
+import { StorageService } from 'src/managers/StorageService';
 
 @Component({
   selector: 'app-profile',
@@ -7,14 +10,32 @@ import { SessionManager } from 'src/managers/SessionManager';  // Importar el Se
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  user: any;
 
-  user: string = '';
+  constructor(
+    private router: Router, 
+    private logoutUseCase: UserLogoutUseCase,
+    private cancelAlertService: CancelAlertService,
+    private storageService: StorageService
+  ) {}
 
-  constructor(private sessionManager: SessionManager) { } 
-
-  ngOnInit() {
-    this.user = this.sessionManager.getUser();  
+  ngOnInit() {}
+  async ionViewDidEnter() {
+    this.user = await this.storageService.get('user');
+    if (!this.user) {
+      console.log('No se encontraron datos del usuario.');
+    }
   }
 
+  async onSignOutButtonPressed() {
+    this.cancelAlertService.showAlert(
+      'Cerrar sesión',
+      '¿Estás seguro de que quieres cerrar sesión?',
+      async () => {
+        this.logoutUseCase.performLogout();
+        this.router.navigate(['/splash']);
+      },
+      () => { }
+    );
+  }
 }
-
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { SessionManager } from 'src/managers/SessionManager';
+//import { UserLogoutUseCase } from '../use-cases/user-logout.user-case';
+import { CancelAlertService } from 'src/managers/CancelAlertService';
+import { StorageService } from 'src/managers/StorageService';
 
 @Component({
   selector: 'app-tabs',
@@ -7,12 +9,20 @@ import { SessionManager } from 'src/managers/SessionManager';
   styleUrls: ['./tabs.page.scss'],
 })
 export class TabsPage implements OnInit {
+  user: any;
 
-  user: string = '';
+  constructor(
 
-  constructor(private sessionManager: SessionManager) {}
+    //private logoutUseCase: UserLogoutUseCase,
+    private cancelAlertService: CancelAlertService,
+    private storageService: StorageService
+  ) {}
 
-  ngOnInit() {
-    this.user = this.sessionManager.getUser();
+  ngOnInit() {}
+  async ionViewDidEnter() {
+    this.user = await this.storageService.get('user');
+    if (!this.user) {
+      console.log('No se encontraron datos del usuario.');
+    }
   }
 }
