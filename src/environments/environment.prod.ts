9c47a874b0fd783e8e042e76151014c8b40ecc31
GisselleAliagaSaavedra <gias0201@gmail.com>
2024-12-01 01:57:37 -0300

    Se logra abrir en androidStudio y usar el modo desarrollador en el celu, pero se cierra al seg en el celu

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
   googleMapsApiKey: 'AIzaSyAAOa1-_x4NYFBUw5FcNijjW8rptGFguiE',
+  firebaseConfig: {
+    apiKey: 'AIzaSyCHPG3bGdFdvgLVdzxiPsYV-k252RpvGIM',
+    authDomain: 'eventura-app.firebaseapp.com',
+    projectId: 'eventura-app',
+    storageBucket: 'eventura-app.firebasestorage.app',
+    messagingSenderId: '978567330481',
+    appId: '1:978567330481:web:81db32c1f43f37188dc090',
+  },
 };
